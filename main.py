7af6bc313f8da986edb38ed6f691685970112f13
Dan <guitarman.dan29@gmail.com>
2022-11-14 17:30:45 -0500

    progress bar and game automation

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from src.cribbage_game import CribbageGame
-from src.player import RandomPlayer, HumanPlayer
+import src.player as player
+from tqdm import tqdm
 
 if __name__ == '__main__':
-    player_one = RandomPlayer('Test 1')
-    player_two = HumanPlayer('Test 2')
+    player_one = player.NaivePlayer('Test 1')
+    player_two = player.RandomPlayer('Test 2')
+    player_one_wins = 0
+    player_two_wins = 0
     game = CribbageGame(player_one, player_two)
-    while (game.get_winner() == None):
-        # Initialize round
-        game.initialize_round()
 
-        # Deal cards
-        game.deal_cards()
+    for i in tqdm(range(1_000_000)):
+        while (game.get_winner() == None):
+            # Initialize round
+            game.initialize_round()
 
-        # Get players to select discards
-        game.handle_discards()
+            # Deal cards
+            game.deal_cards()
 
-        # Score non-dealer hand
-        game.score_non_dealer()
+            # Get players to select discards
+            game.handle_discards()
 
-        # If no winner, score dealer hand (with top card)
-        if game.get_winner() == None:
-            game.score_dealer()
-            game.score_crib()
+            # Score non-dealer hand
+            game.score_non_dealer()
 
-        # Reset game (collect all cards)
-        game.reset_game()
+            # If no winner, score dealer hand (with top card)
+            if game.get_winner() == None:
+                game.score_dealer()
+                game.score_crib()
 
-    print('The winner is', game.get_winner().name, '!')
+            # Reset game (collect all cards)
+            game.reset_game()
+
+        if game.get_winner() == player_one:
+            player_one_wins += 1
+        else:
+            player_two_wins += 1
+        game.start_new_game()
+
+    print('The final score was: ')
+    print(str(player_one) + ': ' + str(player_one_wins))
+    print(str(player_two) + ': ' + str(player_two_wins))
